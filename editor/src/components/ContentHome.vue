036af54ea5d971fd348173a4999de2292b097ef8
AnthonyTaylor <AnTaylor@Northamptonshire.gov.uk>
2020-08-10 19:12:42 +0100

    Adding basic postcode layout

--- editor/src/components/ContentHome.vue
+++ editor/src/components/ContentHome.vue
@@ -1,8 +1,7 @@
-<template>
-  <div>
+<template >
+  <div class="ma-6 pa-6">
     <notice-editor :notice="activeNotice" v-if="activeNotice"/>
-    <v-content v-else>
-      <div class="ma-6 pa-6">
+    <v-main v-else>
         <v-row>
           <v-col>
             <h1>Lockdown notices</h1>
@@ -26,27 +25,44 @@
             <v-btn @click="newNotice">Add</v-btn>
           </v-col>
         </v-row>
-      </div>
-      <v-row>
-        <v-col>
-          <h1>Postcode groups</h1>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col class="md-4">
-          <v-text-field label="Filter" />
-          <v-textarea/>
-        </v-col>
-        <v-col class="md-8">
-          <v-text-field label="Group name" />
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-
-        </v-col>
-      </v-row>
-    </v-content>
+        <v-row>
+          <v-col>
+            <h1>Postcode groups</h1>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col class="col-4">
+            <v-text-field label="Filter" />
+            <v-list shaped>
+              <v-list-item-group color="primary">
+                <v-list-item
+                  v-for="postcodeGroup in postcodeGroups"
+                  :key="postcodeGroup.id"
+                  @click="loadGroup(postcodeGroup)"
+                >
+                  <v-list-item-content>
+                    <v-list-item-title v-text="postcodeGroup.name"></v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list-item-group>
+            </v-list>
+          </v-col>
+          <v-col class="col-8">
+          
+            <div v-if="activeGroup">
+              <v-textarea 
+              v-model="activeGroup.postcodes"
+              rows = "10"
+              auto-grow>   
+              </v-textarea>
+            </div>
+            <div v-else>
+              Select a group ...
+            </div>
+          </v-col>
+          <v-btn @click="newGroup">Add</v-btn>
+        </v-row>
+    </v-main>
   </div>
 </template>
 
@@ -72,9 +88,28 @@ export default {
           postcodes: ['CB4 2XY', 'TN22 3BU']
         }
       ],
-      activeNotice: null
+      postcodeGroups:[
+        {
+          id: '1',
+          name: 'NCC properties',
+          postcodes: ['NN1 1ED', 'NN16 0LL']
+        },
+         {
+          id: '2',
+          name: 'NCC Schools',
+          postcodes: ['NN9 6PA']
+        }
+      ],
+      activeNotice: null,
+      activeGroup: null
     }
   },
+
+
+  computed:{
+
+  },
+
   methods: {
     newNotice() {
       this.notices.push({
@@ -86,13 +121,23 @@ export default {
       })
     },
     loadNotice(notice) {
-      this.activeNotice = notice
+      this.activeNotice = notice;
     },
     deleteNotice(id) {
       id
       // comfirm delete
       // delete on API
       // then delete from collection
+    },
+    newGroup(){
+      this.postcodeGroups.push({
+        id: 'new guid',
+        name: 'New group',
+        content: ''
+      })
+    },
+    loadGroup(group){
+      this.activeGroup = group
     }
   }
 }
