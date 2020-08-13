52a320511dcad16def8eeae918cd7c8ed55561e4
Benedict <benedict@Benedicts-Work-MacBook-Pro.local>
2020-08-13 23:40:41 +0100

    Added link to LGSS Digital website

--- editor/src/App.vue
+++ editor/src/App.vue
@@ -4,7 +4,11 @@
       app
       color="primary"
       dark>
-      <h1>Local Lockdown Editor</h1> <h3 style="padding-left: 5px;">by LGSS Digital</h3>
+      <h1>Local Lockdown Editor</h1> 
+      <h3 style="padding-left: 5px;">by LGSS Digital</h3>
+      <v-btn class="pa-0" min-width="32px" href="https://www.lgss-digital.co.uk" target="_blank" text>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
       <v-spacer></v-spacer>
     </v-app-bar>
     <v-main>
