6b26cd79c2feebe613ac3afc7194268300257e7f
Benedict <benedict@Benedicts-Work-MacBook-Pro.local>
2020-08-17 12:50:54 +0100

    Basic player UI

--- player/src/App.vue
+++ player/src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
+  <div id="app" class="govuk-template__body">
+    <Header />
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="govuk-width-container ">
+      <router-view/>
+    </div>
+    <Footer/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Footer
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+  @import "node_modules/govuk-frontend/govuk/all"
 </style>
