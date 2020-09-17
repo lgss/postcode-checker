99e42e0285d2c37bcf30153d6c23910535a76d4f
SturatParker <>
2020-09-17 11:05:19 +0100

    Update notice.js

--- api/src/notice.js
+++ api/src/notice.js
@@ -1,15 +1,42 @@
 const { response: createResponse, formatPostcode } = require("./util");
 const db = require("./db");
+const tableName = process.env.TABLE_NAME;
+const { pushNotice, pullNotice } = require("./postcode");
+
+const syncPostcodes = (updateNotice, currentNotice) => {
+    console.log("Syncing");
+    const set = updateNotice.postcodes;
+    const remove = currentNotice.postcodes.filter(
+        (el) => !updateNotice.postcodes.includes(el)
+    );
+
+    delete updateNotice.postcodes;
+
+    return Promise.all([
+        ...set.map((p) => pushNotice(p, updateNotice)),
+        ...remove.map((p) => pullNotice(p, updateNotice.id)),
+    ]);
+};
 
 exports.save = (event, context, callback) => {
     reqBody = JSON.parse(event.body);
-    let item = {
+    let notice = {
         ...reqBody,
         id: event.pathParameters.id,
         sortkey: "notice",
         specificity: (reqBody.postcodes || []).length,
     };
-    return db.simple_create(event, item, callback);
+    let params = {
+        TableName: tableName,
+        Key: {
+            id: notice.id,
+        },
+    };
+    return db.dynamo
+        .get(params)
+        .promise()
+        .then((res) => syncPostcodes(notice, res.Item))
+        .then((res) => db.simple_create(event, notice, callback));
 };
 
 exports.get = (event, context, callback) => {
@@ -20,57 +47,68 @@ exports.get = (event, context, callback) => {
         const params = {
             FilterExpression: "sortkey = :input",
             ExpressionAttributeValues: { ":input": "notice" },
-            ProjectionExpression: "id, notice_name, notice_default, content, postcodes"
-        }
+            ProjectionExpression:
+                "id, notice_name, notice_default, content, postcodes",
+        };
         return db.advanced_scan(event, params, callback);
     }
 };
 
 exports.delete = (event, context, callback) => {
-    return db.simple_delete(event, event.pathParameters.id, callback)
-}
+    return db.simple_delete(event, event.pathParameters.id, callback);
+};
 
 exports.byPostcode = (event, callback) => {
-    const failed = (err) => { 
+    const failed = (err) => {
         console.log(`SCAN FAILED WITH ERROR: ${err}`);
-        return createResponse(500, JSON.stringify(err))
-    }
+        return createResponse(500, JSON.stringify(err));
+    };
 
-    const success = (data) => { 
+    const success = (data) => {
         return {
             statusCode: 200,
-            body: data.Items.map((x) => `<notice>${x.content}</notice>`).join('\n'),
+            body: data.Items.map((x) => `<notice>${x.content}</notice>`).join(
+                "\n"
+            ),
             headers: {
                 "Access-Control-Allow-Origin": "*",
                 "Content-Type": "text/html; charset=UTF-8",
-                "Postcode": pc
-            }
-        }
-    }
-    
+                Postcode: pc,
+            },
+        };
+    };
+
     let pc = formatPostcode(event.pathParameters.postcode);
-    console.log(`"${event.pathParameters.postcode}" became ${pc}`)
+    console.log(`"${event.pathParameters.postcode}" became ${pc}`);
     const params = {
         TableName: process.env.TABLE_NAME,
         ProjectionExpression: "content",
-        FilterExpression: "sortkey = :sortkey and contains(postcodes, :postcode)",
+        FilterExpression:
+            "sortkey = :sortkey and contains(postcodes, :postcode)",
         ExpressionAttributeValues: { ":postcode": pc, ":sortkey": "notice" },
-    }
+    };
 
-    return db.dynamo.scan(params).promise()
-    .then((data) => {
-        if (data.Count === 0) {
-            const params2 = {
-                TableName: process.env.TABLE_NAME,
-                ProjectionExpression: "content",
-                FilterExpression: "sortkey = :sortkey and notice_default = :def",
-                ExpressionAttributeValues: { ":def": true, ":sortkey": "notice" },
-            }
-            return db.dynamo.scan(params2).promise()
-            .then(success)
-            .catch(failed)
-        }
-        else return success(data)
-    })
-    .catch(failed)
-}
+    return db.dynamo
+        .scan(params)
+        .promise()
+        .then((data) => {
+            if (data.Count === 0) {
+                const params2 = {
+                    TableName: process.env.TABLE_NAME,
+                    ProjectionExpression: "content",
+                    FilterExpression:
+                        "sortkey = :sortkey and notice_default = :def",
+                    ExpressionAttributeValues: {
+                        ":def": true,
+                        ":sortkey": "notice",
+                    },
+                };
+                return db.dynamo
+                    .scan(params2)
+                    .promise()
+                    .then(success)
+                    .catch(failed);
+            } else return success(data);
+        })
+        .catch(failed);
+};
